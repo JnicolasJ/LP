b09539df3550c1f4e34e4a063cef083cf2246dcc
JnicolasJ <jose.jimenez@ucsp.edu.pe>
2015-09-14 23:31:06 -0500

    parser programing in java

---
+++ lexjav/parser/src/parser/Parser.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parser;
+
+/**
+ *
+ * @author Nico
+ */
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+enum CharClass
+{
+    letter(1), digit(2), floa(3), unknow(4),invalid(5);
+}
+public class Parser {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
