644ccbd79cc181b91e14494a8cd4f660a3a26ed1
JnicolasJ <jose.jimenez@ucsp.edu.pe>
2015-10-05 23:44:09 -0500

    problem 6 in java

---
+++ ProblemSet5/problem6.java
@@ -0,0 +1,12 @@
+public class problem6{
+
+	public static void main(String[] args)
+	{
+		for(int i=0; i<10; i++)
+		{
+		}
+		i = 5;
+		//igual que en el caso anterior  la variable i=5 esta fuera del alcance de la isntanciacion de i dentro del ambito del for
+	}
+
+}
