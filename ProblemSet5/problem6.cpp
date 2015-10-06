d9131aebabe19c62d1702f941e85dedc2d2fe778
JnicolasJ <jose.jimenez@ucsp.edu.pe>
2015-10-05 23:40:20 -0500

    problem 6 in c++

---
+++ ProblemSet5/problem6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	for(int i=0; i<10; i++)
+	{}
+	i =3;
+	// en esta ultima parte la variable i no es reconocida por el conpilador ya que esta fuera del alcanse
+	//de la inicializacion de i dentro del ambito del for
+return 0;
+}
