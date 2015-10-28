7eafea8aa192e819ba063449e8117353f5b24d40
JnicolasJ <jose.jimenez@ucsp.edu.pe>
2015-10-27 23:00:01 -0500

    error al mezclar tipos primitivos

--- dataTypes/problem1.c
+++ dataTypes/problem1.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
-//que clase de tipos primitivos se pueden mezclar
+
 int main (int argc, char * argv[]) {
-
+//que clase de tipos primitivos se pueden mezclar
+	printf("1.- mezclando tipos  int - long int\n");
      int x = 5;
-
      long int y= 973562454554,z;
-
- 
-
      z = x;
 
      x = y;
 
-        
-
-     printf("the value of z= %ld \n the value of x= %d", z, x);  
-        
+     printf("el valor de  z= %ld \nel valor de  x= %d", z, x); 
+     printf("\n");       
 
 //es compatible la asignacion de un int hacia un long int pero no al revez
+  printf("es compatible la asignacion de un int hacia un long int pero no al revez \n");  
+  	printf("tipos double - float\n");
+    double a = 3.145965932;
+	float  c = 3.14, b;
+ 
+	b = a;
+	a = c;
+ 
+	printf("el valor de b = %f\nel valor de a = %lf", b, a);
+	printf("\n");       
      return 0;
 
 } 
