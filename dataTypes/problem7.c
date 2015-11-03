42105bca14ca4620a9018ece71496e7d0e641c0d
JnicolasJ <jose.jimenez@ucsp.edu.pe>
2015-11-03 00:22:12 -0500

    problem 7 diferencia puntero subindice

---
+++ dataTypes/problem7.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <time.h>
+#include <iostream>
+
+using namespace std;
+
+int ar[50];
+void llenar()
+{
+	for(int i=0;i<50;i++)
+		ar[i]=1;
+}
+
+void funcion()
+{
+	
+	for(int i=0;i<50;i++)
+		ar[i]=0;	
+}
+
+void funcion2()
+{
+
+	for(int* puntero = &ar[0];puntero != &ar[50];puntero++)
+		(*puntero)=0;
+}
+
+int main(int argc, char const *argv[])
+{
+	llenar();
+	float ini = clock();
+	for(int i=0; i<100000;i++)
+	{
+
+		funcion();
+	}
+	float end = clock();
+
+	cout<<"tiempo de la primera funcion"<<(end - ini) / ((double)CLOCKS_PER_SEC)<<endl;
+
+	llenar();
+	float ini1 = clock();
+	for(int i=0; i<100000;i++)
+	{
+
+		funcion2();
+	}
+	float end1 = clock();
+
+	cout<<"tiempo de la segunda funcion"<<(end1 - ini1) / ((double)CLOCKS_PER_SEC)<<endl;
+	
+	return 0;
+}
+
+/*punteros heap dinamica de pila
+tipo de dato que almacena direciciones de memoria
+inicializamos el puntero con la primera direccion del ar
+cuando hacemos la aritmetica de punteros al hace
+puntero ++ lo que hacemos es ir a la siguiente direccion de memoria
+luego con el operador *(puntero) accedemos al contenido de la direccion de memoria
+los punteros son dinamicos
+*/
+/*
+subindice arr[i] accedemos directamente al contenido de la celda de la memoria 
+*/
