220f8af07ccfb5076fd12caae4f39bb5abad5162
JnicolasJ <jose.jimenez@ucsp.edu.pe>
2015-11-03 00:21:04 -0500

    asignacion A=B lenguaje java

---
+++ dataTypes/problem10.java
@@ -0,0 +1,19 @@
+public class problem10{
+
+	public static void main(String[] args)
+	{
+		int x;
+		int a;
+		int b;
+		double y = 15.35;
+		double z = 15.35;
+		a = y*z;
+		x = y+z;
+		b = y;
+		System.out.println(" valor de b es: " + b);
+		System.out.println(" valor de a es: " + a);
+		System.out.println(" valor de x es: " + x);
+	}
+}
+
+//java no puedo convertir double a un int
