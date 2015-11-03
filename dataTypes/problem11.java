659b667d243b57c27a9e57ccc2f31d985f8bf297
JnicolasJ <jose.jimenez@ucsp.edu.pe>
2015-11-03 09:11:33 -0500

    assignment java - ada

---
+++ dataTypes/problem11.java
@@ -0,0 +1,13 @@
+public class problem11{
+
+	public static void main(String[] args)
+	{
+		int x= 24;
+		int a= 12;
+		float b;
+		
+		b= x/a;
+		System.out.println(" valor de b es: " + b);
+		
+	}
+}
